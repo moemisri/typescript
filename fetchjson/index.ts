1fe74d51d2b1b16f5b2301264cb8d5e5a34433d2
Moe Misri <mohammed.masry74@gmail.com>
2023-06-25 13:00:06 +0300

    priting out tasks - typescript fiist day

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,3 +1,20 @@
 import axios from "axios";
-const url = "https://jsonplaceholder.typicode.com/todos?userId=3";
-axios.get(url).then((response) => console.log(response.data));
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+  logTodo(id, title, finished);
+});
+const logTodo = (id: number, title: string, finished: boolean) => {
+  console.log(`
+    task number ${id}: ${title} 
+    is ${finished ? "finished" : "not finished"} 
+  `);
+};
